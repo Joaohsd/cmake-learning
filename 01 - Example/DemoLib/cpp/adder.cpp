f2b3fa2d135802a34c73e37f2408f9f0becf888d
João Delfino <joaohsdelfino@gmail.com>
2024-01-02 22:45:12 -0300

    First example creating a static library in c++

---
+++ 01 - Example/DemoLib/cpp/adder.cpp
@@ -0,0 +1,9 @@
+#include "adder.h"
+
+int myNamespace::add(int a, int b){
+    return a+b;
+}
+
+float myNamespace::add(float a, float b){
+    return a+b;
+}
---
+++ 01 - Example/DemoProject/cpp/main.cpp
@@ -0,0 +1,7 @@
+#include "../../DemoLib/cpp/adder.h"
+#include <iostream>
+
+int main(){
+    std::cout << myNamespace::add(2, 3) << std::endl; 
+    return 0;
+}
