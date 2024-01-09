339feb0259069972234f4209bc32b8075e4dd514
João Delfino <joaohsdelfino@gmail.com>
2024-01-08 22:35:52 -0300

    Added .h file in order to use external definitions in my program, like: versions, paths, etc.

--- 02 - Example/DemoProject/cpp/main.cpp
+++ 02 - Example/DemoProject/cpp/main.cpp
@@ -1,7 +1,13 @@
-#include "adder.h"
+#include "../../DemoLib/cpp/adder.h"
 #include <iostream>
+#include <demoprojectConfig.h>
 
-int main(){
-    std::cout << myNamespace::add(2, 3) << std::endl; 
+int main(int argc, char* argv[]){
+    std::cout << "Project started..." << std::endl;
+
+    std::cout << "Project " << argv[0] << " version: " << DEMOPROJECT_VERSION_MAJOR << "." << DEMOPROJECT_VERSION_MINOR << std::endl;
+    
+    std::cout << myNamespace::add(2, 3) << std::endl;
+
     return EXIT_SUCCESS;
 }
